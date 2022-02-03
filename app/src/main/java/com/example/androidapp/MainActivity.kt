371aa0f25a9cad95d68ab075879587442804c5e6
Lozerd <dmisha29411@gmail.com>
2022-02-04 00:56:08 +0300

    added SecondActivity.kt redirect and intent on it

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -28,6 +29,13 @@ class MainActivity : AppCompatActivity() {
     fun reset(view: View){
         val textView: TextView = findViewById(R.id.textView)
         textView.text = "0"
+    }
+
+    fun redirect(view: View) {
+        val secondActivity = Intent(this, SecondActivity::class.java)
+        val count: Int = Integer.parseInt(findViewById<TextView>(R.id.textView).text.toString())
+        secondActivity.putExtra(SecondActivity.TOTAL_COUNT, count)
+        startActivity(secondActivity)
     }
 
 }
---
+++ app/src/main/java/com/example/androidapp/SecondActivity.kt
@@ -0,0 +1,34 @@
+package com.example.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toolbar
+
+class SecondActivity : AppCompatActivity() {
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        supportActionBar?.apply {
+            title = "Toolbar Back Button"
+
+            // show back button on toolbar
+            // on back button press, it will navigate to parent activity
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+        showTotalCount()
+    }
+
+    fun showTotalCount(){
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+        findViewById<TextView>(R.id.textView3).text = getString(R.string.count, count)
+    }
+}
