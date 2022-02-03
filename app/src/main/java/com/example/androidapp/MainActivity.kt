5059c0e9e8a21ec004f1a131ae1fd91a2741fe12
Lozerd <dmisha29411@gmail.com>
2022-02-03 23:55:44 +0300

    counter done

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -4,26 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private var textView: TextView = findViewById(R.id.textView)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        this.textView = findViewById(R.id.textView)
         setContentView(R.layout.activity_main)
     }
 
     fun add(view: View){
-        var count: Int = Integer.parseInt(this.textView.text.toString())
+        val textView: TextView = findViewById(R.id.textView)
+        var count: Int = Integer.parseInt(textView.text.toString())
         count++
         textView.text = count.toString()
     }
 
     fun delete(view: View) {
-        var count: Int = Integer.parseInt(this.textView.text.toString())
+        val textView: TextView = findViewById(R.id.textView)
+        var count: Int = Integer.parseInt(textView.text.toString())
         count--
         textView.text = count.toString()
     }
+
+    fun reset(view: View){
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = "0"
+    }
+
 }
